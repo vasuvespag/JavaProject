0fb7b5633f48dfdc035a4a08731c981d21ba6a49
Ashwathi-25 <ashwathidiana@gmail.com>
2022-12-07 13:13:16 +0530

    Commit

--- src/test/JavaExceptionExample.java
+++ src/test/JavaExceptionExample.java
@@ -8,19 +8,16 @@ public class JavaExceptionExample{
              //a[5]=30/0;    
              System.out.println(a[10]);  
             }    
-        
+        catch(ArrayIndexOutOfBoundsException e)  
+        {  
+         System.out.println("ArrayIndexOutOfBounds Exception occurs");  
+        }    
         catch(Exception e)  
         {  
          System.out.println("Parent Exception occurs");  
         }   
-            catch(ArithmeticException e)  
-               {  
-                System.out.println("Arithmetic Exception occurs");  
-               }    
-            /*catch(ArrayIndexOutOfBoundsException e)  
-               {  
-                System.out.println("ArrayIndexOutOfBounds Exception occurs");  
-               }    */
+              
+
                       
             System.out.println("rest of the code");    
  }  
